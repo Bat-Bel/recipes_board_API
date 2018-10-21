75aca5accca9a9b63c9bb3cc7387c9114d9eb046
Yasser Batas <yasser.batas@gmail.com>
2018-10-21 05:02:01 -0500

    add signup endpoint

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,36 @@ var router = express.Router();
 var knex = require('knex');
 var knexFile = require('../knexfile');
 var db = knex(knexFile[process.env.NODE_ENV || 'development']);
+const uuid = require('uuid/v4');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.post('/signup', function(req, res, next) {
+  const {
+    user_name,
+    password,
+    first_name,
+    last_name,
+    email,
+    user_image,
+    phone_number
+  } = req.body;
+
+  db('users')
+    .insert({
+      uuid: uuid(),
+      user_name,
+      password,
+      first_name,
+      last_name,
+      email,
+      user_image,
+      phone_number
+    })
+    .then(data => {
+      res.json({ success: 'User has been registered correctly' });
+    })
+    .catch(error => {
+      res.json({ error: 'Has been an error during the registration process' });
+    });
 });
 
 module.exports = router;
