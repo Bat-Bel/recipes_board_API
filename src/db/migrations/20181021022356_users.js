938cec83cf8bb135033c275ffe719d0bf568a3bb
Yasser Batas <yasser.batas@gmail.com>
2018-10-21 03:17:42 -0500

    add seeds for users table

--- src/db/migrations/20181021022356_users.js
+++ src/db/migrations/20181021022356_users.js
@@ -2,16 +2,16 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('users', function(t) {
     t.uuid('id').primary();
-    t.string('username', 50).unique().notNullable();
+    t.string('user_name', 50).unique().notNullable();
     t.string('first_name', 200).notNullable();
     t.string('last_name', 200).notNullable();
-    t.integer('email', 200).unique().notNullable();
+    t.string('email', 200).unique().notNullable();
     t.string('user_image', 256);
-    t.integer('phone_number', 15);
+    t.string('phone_number', 15);
     t.timestamps();
   });
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTableIfExist('users');
 };
--- src/db/seeds/users.js
+++ src/db/seeds/users.js
@@ -1,13 +1,28 @@
+const uuid = require('uuid/v4');
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
+  return knex('users').del().then(function() {
+    // Inserts seed entries
+    return knex('users').insert([
+      {
+        id: uuid(),
+        user_name: 'yasskate',
+        first_name: 'Yasser',
+        last_name: 'Batas',
+        email: 'yassk8@gmail.com',
+        user_image: '',
+        phone_number: '3121234826'
+      },
+      {
+        id: uuid(),
+        user_name: 'sambelmor',
+        first_name: 'Dzoara',
+        last_name: 'Bello',
+        email: 'sambelmor@gmail.com',
+        user_image: '',
+        phone_number: '3121113267'
+      },
+    ]);
+  });
 };
