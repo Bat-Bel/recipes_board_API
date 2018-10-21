7efad33f5614b0bc183ec229e060923e781d84a7
Yasser Batas <yasser.batas@gmail.com>
2018-10-21 05:02:43 -0500

    add knex and database connection setup

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
+var knex = require('knex');
+var knexFile = require('../knexfile');
+var db = knex(knexFile[process.env.NODE_ENV || 'development']);
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', (req, res, next) => {
+  db('users').select('*').then(users => {
+    res.json(users);
+  });
+});
+
+router.get('/:id', (req, res, next) => {
+  const { id } = req.params;
+  db('users').select('*').where({ id }).then(user => {
+    if (user.length > 0) {
+      res.json(user[0]);
+    }
+
+    res.json({ error: `There are not users with this id [${id}]` });
+  });
 });
 
 module.exports = router;
