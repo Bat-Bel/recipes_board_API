b3c26b85bf7225dbee4c7e5bb0bd8c41a7191020
alejandro <alejandro.mendez@varangard.com>
2018-10-27 23:36:54 -0500

    Token is already returned to the user

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,10 @@ var knex = require('knex');
 var knexFile = require('../knexfile');
 var db = knex(knexFile[process.env.NODE_ENV || 'development']);
 const uuid = require('uuid/v4');
+const jwt = require('jsonwebtoken');
 
+// to test registration by curl:
+// user_name=sskate&password=test123&first_name=sser&last_name=Batas&email=yassk8@mail.com&user_image=%20&phone_number=3121234826
 router.post('/signup', function(req, res, next) {
   const {
     user_name,
@@ -28,7 +31,19 @@ router.post('/signup', function(req, res, next) {
       phone_number
     })
     .then(data => {
-      res.json({ success: 'User has been registered correctly' });
+      const JWTToken = jwt.sign({
+        email: user_name,
+        _id: uuid
+      },
+      'secret',
+      {
+        expiresIn: '2h'
+      });
+      
+      return res.status(200).json({  
+          success: 'User has been registered correctly',
+          token: JWTToken 
+      });
     })
     .catch(error => {
       res.json({ error: 'Has been an error during the registration process' });
